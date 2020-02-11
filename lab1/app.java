458610a58fb9a47f5ffd9c1628513c6da4da729c
adi <aditya.dasgupta1999@gmail.com>
2020-02-10 18:03:13 -0600

    few changes

--- lab1/app.java
+++ lab1/app.java
@@ -1,5 +1,5 @@
 package Sort;
-
+import Sort.Sorting;
 import java.awt.*;
 
 import javax.swing.JFrame;
@@ -13,6 +13,7 @@ import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.border.EtchedBorder;
 
 public class app {
 
@@ -23,7 +24,11 @@ public class app {
 	//Labels
 	JLabel algorithm = new JLabel("Algorithm");
 	JLabel compare = new JLabel("Compare");
+	JLabel size = new JLabel("Size of List?");
 	
+	//Buttons
+	JButton shuffle = new JButton("Shuffle!");
+	JButton sort = new JButton("Sort!");
 	/**
 	 * Launch the application.
 	 */
@@ -61,12 +66,50 @@ public class app {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setResizable(false);
 		jf.setLocationRelativeTo(null);
-		jf.getContentPane().setLayout(null);
+		//jf.getContentPane().setLayout(null);
+		jf.getContentPane().setLayout(new BoxLayout(jf.getContentPane(), BoxLayout.X_AXIS));
+		//panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		
+		jf.getContentPane().add(tools);
 		
 		//Algorithm Select
 		algorithm.setHorizontalAlignment(JLabel.CENTER);
-		algorithm.setBounds(40,20,100,25);
+		algorithm.setBounds(90,119,52,14);
+		tools.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		tools.setLayout(null);
 		tools.add(algorithm);
+		
+		//Size of List 
+		size.setHorizontalAlignment(JLabel.LEFT);
+		size.setBounds(90,82,66,14);
+		tools.add(size);
+		
+		//Shuffle Button
+		shuffle.setBounds(90,145,69,26);
+		tools.add(shuffle);
+		
+		//Sort Button
+		sort.setBounds(90,183,100,25);
+		tools.add(sort);
+		
+		
+		shuffle.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Hello");
+				//shuffleList();
+				//reset();
+			}
+		});
+		
+		sort.addActionListener(new ActionListener() {
+			//@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Yo");
+				//sort
+				//reset();
+			}
+		});
 		/*
 		jf = new JFrame();
 		jf.setBounds(100, 100, 450, 300);
