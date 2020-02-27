69764f7f37ade80ac2fd2776cb383a75447d1a74
adi <aditya.dasgupta1999@gmail.com>
2020-02-27 12:23:23 -0600

    added comments to bruteforce.py

--- lab2/bruteforce.py
+++ lab2/bruteforce.py
@@ -1,20 +1,23 @@
 #Team name: null
 #Team members: Aditya Dasgupta(seat 14) and Devyani Gauri(Seat 25)
 
+#running time -> O(n^2)
 def maxSubArr(arr):
-    largest = 0
+    #set the largest and the current to 0
+    largest = 0 
     current = 0
 
-    for i in range(0,len(arr)):
+    for i in range(0,len(arr)):   #the index of the first element of a subarray is denoted by i
         j = i
-        while j<=len(arr):
-            curArr = arr[i:j]
+        while j<=len(arr):        #after every iteration of the while loop, the size of the subarray is increased by 1.
+            curArr = arr[i:j]     #creation of the subarray
             print(curArr)
-            current = sum(curArr) #current subarray
-            if current > largest:
-                largest = current
+            current = sum(curArr) #sum of current subarray
+            if current > largest: #checks if current subarray is larger than the last recorded largest subarray
+                largest = current #sets largest to current
             j = j+1
     return largest
 
+#test case [should return 875]
 l1 = [7,20,-300,65,810,-21]
 maxSubArr(l1)
